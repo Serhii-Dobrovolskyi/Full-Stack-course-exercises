8d0457df5327c3086ddee05c55e317b84a52a088
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2024-12-22 16:28:24 +0200

    added the unicafe task step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,17 +4,45 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+
+  const calculateAverage = (pos, neg, total) => (pos - neg) / total;
+
+  const goodClick = () => {
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    const updatedAll = updatedGood + neutral + bad;
+    setAll(updatedAll);
+    setAverage(calculateAverage(updatedGood, bad, updatedAll));
+  };
+  const neutralClick = () => {
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    const updatedAll = good + updatedNeutral + bad;
+    setAll(updatedAll);
+    setAverage(calculateAverage(good, bad, updatedAll));
+  };
+  const badClick = () => {
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    const updatedAll = good + neutral + updatedBad;
+    setAll(updatedAll);
+    setAverage(calculateAverage(good, updatedBad, updatedAll));
+  };
 
   return (
     <div>
       <h2>give feedback</h2>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={goodClick}>good</button>
+      <button onClick={neutralClick}>neutral</button>
+      <button onClick={badClick}>bad</button>
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
     </div>
   );
 };
