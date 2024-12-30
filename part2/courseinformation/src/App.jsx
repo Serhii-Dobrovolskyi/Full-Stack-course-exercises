1746db15dc4221a8b81fa905133c63f5a1671573
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2024-12-30 20:18:43 +0200

    created the task 2.1

--- part2/courseinformation/src/App.jsx
+++ part2/courseinformation/src/App.jsx
@@ -1,55 +1,65 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
 };
 
-const Part = (props) => {
+const Part = ({ name, exercises }) => {
   return (
     <p>
-      {props.name} {props.exercises}
+      {name} {exercises}
     </p>
   );
 };
 
-const Content = (props) => {
+const Content = ({ parts }) => {
   return (
     <div>
-      {props.parts.map((el,index) => <Part key={index} name={el.name} exercises={el.exercises} />)}
+      {parts.map((el, id) => (
+        <Part key={id} name={el.name} exercises={el.exercises} />
+      ))}
     </div>
   );
 };
 
-const Total = (props) => {
+const Total = ({ parts }) => {
   return (
     <p>
-      Number of exercises{" "}
-      {props.parts.reduce((acc,num)=> acc + num.exercises,0)}
+      Number of exercises {parts.reduce((acc, num) => acc + num.exercises, 0)}
     </p>
   );
 };
 
-const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+const Course = ({ course }) => {
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
+};
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+  return <Course course={course} />;
 };
 
 export default App;
