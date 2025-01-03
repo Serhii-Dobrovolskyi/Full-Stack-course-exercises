acdce47ddb0177150699a5b4c6302b697d044920
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2025-01-03 17:52:34 +0200

    added the task 2.7

--- part2/thephonebook/src/App.jsx
+++ part2/thephonebook/src/App.jsx
@@ -1,8 +1,51 @@
+import { useState } from "react";
 
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas" },
+    { name: "Alina" },
+    { name: "Serhii" },
+  ]);
+  const [newName, setNewName] = useState("");
 
-function App() {
+  const addName = (e) => {
+    e.preventDefault();
+    if (newName) {
+      if(persons.find(item=>item.name===newName)){
+        alert(`${newName} is already added to phonebook`)
+        return
+      }
+      const newUser = {
+        name: newName,
+      };
+      
+      setPersons(persons.concat(newUser));
+      setNewName("");
+    }
+  };
 
-  return <h2>Test</h2>
-}
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name:{" "}
+          <input
+            value={newName}
+            placeholder="fill the text"
+            onChange={(e) => setNewName(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((el) => (
+        <p key={el.name}>{el.name}</p>
+      ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
