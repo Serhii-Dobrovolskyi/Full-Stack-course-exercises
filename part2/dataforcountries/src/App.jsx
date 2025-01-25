99ef837ce23ea8fb32e0177df0abc1cc4595a737
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2025-01-25 14:30:07 +0200

    added the task 2.18

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -1,6 +1,70 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+const DataOfTheCountry = ({ name, capital, area, languages, flag }) => {
+  return (
+    <>
+      <h2>{name}</h2>
+      <p>capital {capital}</p>
+      <p>area {area}</p>
+      <span>languages</span>
+      <ul>
+        {Object.values(languages).map((language) => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img src={flag} alt="#" />
+    </>
+  );
+};
 
-  return <h1>find counties</h1>
-}
-export default App
+const App = () => {
+  const [value, setValue] = useState("");
+  const [allCountries, setAllCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+      .then((response) => setAllCountries(response.data))
+      .catch(() => console.log("Not found"));
+  }, []);
+
+  const filteredCountries = value
+    ? allCountries.filter((i) =>
+        i.name.common.toLowerCase().includes(value.toLowerCase())
+      )
+    : [];
+
+  return (
+    <div>
+      <p>
+        find countries{" "}
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Enter country..."
+        />
+      </p>
+      {
+        filteredCountries.length > 10 && <p>Too many matches, specify another filter</p>
+      }
+      {
+        filteredCountries.length <= 10 && filteredCountries.length > 1 && (
+          filteredCountries.map((i) => (
+            <p key={i.name.common}>{i.name.common}</p>
+          ))
+        )
+      }
+      {filteredCountries.length === 1 && (
+        <DataOfTheCountry
+          name={filteredCountries[0].name.common}
+          capital={filteredCountries[0].capital}
+          area={filteredCountries[0].area}
+          languages={filteredCountries[0].languages}
+          flag = {filteredCountries[0].flags.png}
+        />
+      )}
+    </div>
+  );
+};
+export default App;
