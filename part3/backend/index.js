157da9e1301fbfb8a4a6bad507f04452f9f3052e
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2025-03-01 18:37:05 +0200

    updated the part 4.1

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -3,31 +3,26 @@ const app = express()
 
 app.use(express.json())
 
-let notes = [
+let persons = [
    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
+      "id": "1",
+      "name": "Arto Hellas",
+      "number": "040-123456"
    },
    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
+      "id": "2",
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523"
    },
    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
+      "id": "3",
+      "name": "Dan Abramov",
+      "number": "12-43-234345"
    },
    {
-      id: "4",
-      content: "Browser can execute only JavaScript",
-      important: false
-   },
-   {
-      id: "5",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
+      "id": "4",
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122"
    }
 
 ]
@@ -35,39 +30,43 @@ let notes = [
 // app.get('/',(request,response)=>{
 //    response.send('<h1>Hello World!</h1>')
 // })
-const generateId = () => {
-   const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0
-   return String(maxId + 1)
-}
-app.get('/api/notes', (request, response) => {
-   response.json(notes)
-})
+// const generateId = () => {
+//    const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0
+//    return String(maxId + 1)
+// }
+// app.get('/api/notes', (request, response) => {
+//    response.json(notes)
+// })
 
-app.post('/api/notes', (request, response) => {
-   const body = request.body
-   if (!body.content) {
-      return response.status(400).json({ error: "content missing" })
-   }
-   const note = {
-      content: body.content,
-      important: Boolean(body.important) || false,
-      id: generateId()
-   }
-   notes = notes.concat(note)
-   response.json(note)
-})
+// app.post('/api/notes', (request, response) => {
+//    const body = request.body
+//    if (!body.content) {
+//       return response.status(400).json({ error: "content missing" })
+//    }
+//    const note = {
+//       content: body.content,
+//       important: Boolean(body.important) || false,
+//       id: generateId()
+//    }
+//    notes = notes.concat(note)
+//    response.json(note)
+// })
 
-app.get('/api/notes/:id', (request, response) => {
-   const id = request.params.id
-   const note = notes.find(note => note.id === id)
+// app.get('/api/notes/:id', (request, response) => {
+//    const id = request.params.id
+//    const note = notes.find(note => note.id === id)
 
-   if (note) {
-      response.json(note)
-   } else {
-      response.status(404).end('<h1>yps.. we have a problem!</h1>')
-   }
+//    if (note) {
+//       response.json(note)
+//    } else {
+//       response.status(404).end('<h1>yps.. we have a problem!</h1>')
+//    }
+// })
+
+app.get('/api/persons', (request, response) => {
+   response.json(persons)
 })
 
 const PORT = 3002
 app.listen(PORT)
-console.log(`Server running on porrt ${PORT}`)
+console.log(`Server running on port ${PORT}`)
