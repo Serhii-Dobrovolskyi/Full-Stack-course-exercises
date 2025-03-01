5ee398e8b2631f84a5020e8e04eeff111956e14a
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2025-03-01 18:38:03 +0200

    updated the part 4.1

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -27,42 +27,6 @@ let persons = [
 
 ]
 
-// app.get('/',(request,response)=>{
-//    response.send('<h1>Hello World!</h1>')
-// })
-// const generateId = () => {
-//    const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0
-//    return String(maxId + 1)
-// }
-// app.get('/api/notes', (request, response) => {
-//    response.json(notes)
-// })
-
-// app.post('/api/notes', (request, response) => {
-//    const body = request.body
-//    if (!body.content) {
-//       return response.status(400).json({ error: "content missing" })
-//    }
-//    const note = {
-//       content: body.content,
-//       important: Boolean(body.important) || false,
-//       id: generateId()
-//    }
-//    notes = notes.concat(note)
-//    response.json(note)
-// })
-
-// app.get('/api/notes/:id', (request, response) => {
-//    const id = request.params.id
-//    const note = notes.find(note => note.id === id)
-
-//    if (note) {
-//       response.json(note)
-//    } else {
-//       response.status(404).end('<h1>yps.. we have a problem!</h1>')
-//    }
-// })
-
 app.get('/api/persons', (request, response) => {
    response.json(persons)
 })
