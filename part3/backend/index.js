123e527ecb5ba4b01bb96aab2f58a0f6aee55682
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2025-03-30 23:59:14 +0300

    added task 3.14

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -38,11 +38,6 @@ let persons = [
       "name": "Dan Abramov",
       "number": "12-43-234345"
    },
-   {
-      "id": "4",
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-   }
 
 
 ]
