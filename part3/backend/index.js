0dd216535399bc73edc7d05abd93fd860cbeb5aa
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2025-04-01 23:38:16 +0300

    updated task 3.15

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -42,6 +42,17 @@ const Person = require('./models/person')
 
 
 // ]
+
+const errorHandler = (error, request, response, next) => {
+   console.error(error.message)
+
+   if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+   }
+
+   next(error)
+}
+
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan('tiny'))
@@ -75,10 +86,13 @@ app.get('/api/persons/:id', (request, response) => {
    })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-   const id = request.params.id
-   persons = persons.filter(person => person.id === id)
-   response.status(204).end('No content')
+app.delete('/api/persons/:id', (request, response, next) => {
+   // const id = request.params.id
+   // persons = persons.filter(person => person.id === id)
+   // response.status(204).end('No content')
+   Person.findByIdAndDelete(request.params.id)
+      .then(result => response.status(204).end())
+      .catch(error => next(error))
 })
 
 morgan.token('body', (request) => JSON.stringify(request.body))
