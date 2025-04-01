7c14dac781cc53d56ea9f9b9e97e6691723fddd9
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2025-04-01 23:47:23 +0300

    added task 3.16

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -113,8 +113,15 @@ app.post('/api/persons', (request, response) => {
       response.json(savedPerson)
    })
 })
+
+const unknownEndpoint = (request, response) => {
+   response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
 const PORT = process.env.PORT
-// const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
    console.log(`Server running on port ${PORT}`)
 })
