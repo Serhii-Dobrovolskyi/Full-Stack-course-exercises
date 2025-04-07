7d1cffccd08cff2c3b65eff0985be50efacab974
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2025-04-07 10:21:43 +0300

    updated task 3.21

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -140,7 +140,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
    console.log(`Server running on port ${PORT}`);
 });
