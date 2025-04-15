a19bf28155222d67dc7104081c8ae1602575064c
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2025-04-15 22:05:03 +0300

    added task 4.6

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -11,6 +11,19 @@ const favoriteBlog = (listOfBlogPosts) => {
    return listOfBlogPosts.reduce((favorite, blog) => blog.likes > favorite.likes ? blog : favorite)
 }
 
+const mostBlogs = (listOfBlogPosts) => {
+   const authorCounts = {}
+   for (let i of listOfBlogPosts) {
+      if (authorCounts[i.author]) {
+         authorCounts[i.author]++
+      } else {
+         authorCounts[i.author] = 1
+      }
+   }
+   const mostObj = Object.entries(authorCounts).reduce((most, item) => item[1] > most[1] ? item : most)
+   return { author: mostObj[0], blogs: mostObj[1] }
+}
+
 module.exports = {
-   dummy, totalLikes, favoriteBlog
+   dummy, totalLikes, favoriteBlog, mostBlogs
 }
