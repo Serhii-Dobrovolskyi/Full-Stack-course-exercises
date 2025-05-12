177d8196735e8a12f1610b00e5d10e85062486e6
Serhii Dobrovolskyi <serhiidobrovolskyi1@gmail.com>
2025-05-12 19:07:41 +0300

    updated task 8.4

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const blogRouter = require('./controllers/notes')
+   const express = require('express')
+   const mongoose = require('mongoose')
+   const blogRouter = require('./controllers/notes')
 
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
+   const config = require('./utils/config')
+   const logger = require('./utils/logger')
+   const middleware = require('./utils/middleware')
 
-const app = express()
-logger.info('connecting to', config.MONGODB_URI)
-mongoose.connect(config.MONGODB_URI).then(() => logger.info('Connected to MongoDB')).catch(err => {
-   logger.error('MongoDB connection error:', err.message)
-})
+   const app = express()
+   logger.info('connecting to', config.MONGODB_URI)
+   mongoose.connect(config.MONGODB_URI).then(() => logger.info('Connected to MongoDB')).catch(err => {
+      logger.error('MongoDB connection error:', err.message)
+   })
 
-app.use(express.static('dist'))
-app.use(express.json())
-app.use(middleware.requestLogger)
+   app.use(express.static('dist'))
+   app.use(express.json())
+   app.use(middleware.requestLogger)
 
-app.use('/api/blogs', blogRouter)
+   app.use('/api/blogs', blogRouter)
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+   app.use(middleware.unknownEndpoint)
+   app.use(middleware.errorHandler)
 
-module.exports = app
+   module.exports = app
--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -5,7 +5,7 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
+test.only('blogs are returned as json', async () => {
    await api.get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
