e8e3a75b60c5cc7eda12affcec64cbde9f82c5f7
Serhii Dobrovolskyi <serhiidobrovolskyi1@gmail.com>
2025-05-13 18:40:06 +0300

    updated task 4.8

--- part4/bloglist/controllers/notes.js
+++ part4/bloglist/controllers/notes.js
@@ -1,15 +1,13 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/note')
-
-blogRouter.get('/', (request, response) => {
-   Blog.find({}).then((blogs) => {
-      response.json(blogs)
-   })
+  
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response) => {
    const blog = new Blog(request.body)
-
    blog.save().then((result) => {
       response.status(201).json(result)
    })
--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -1,16 +1,30 @@
-const { test, after } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
-const app = require('../app')
+const app = require('../app') // твое express-приложение
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-test.only('blogs are returned as json', async () => {
-   await api.get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const initialBlogs = [
+    { title: 'First blog', author: 'Author1', url: 'http://example.com/1', likes: 1 },
+    { title: 'Second blog', author: 'Author2', url: 'http://example.com/2', likes: 2 }
+  ]
+
+  await Blog.insertMany(initialBlogs)
 })
 
-after(async () => {
-   await mongoose.connection.close()
+test('blogs are returned as json and correct number', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body).toHaveLength(2)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
 })
