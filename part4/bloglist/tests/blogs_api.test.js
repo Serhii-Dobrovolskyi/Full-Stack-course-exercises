6c0b71ab93c1df665a6efa162ed2943fe334c8a4
Serhii Dobrovolskyi <serhiidobrovolskyi1@gmail.com>
2025-05-14 23:27:25 +0300

    updated task 4.8

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -1,30 +1,37 @@
+const assert = require('node:assert')
 const mongoose = require('mongoose')
+const { test, after, beforeEach, describe } = require('node:test')
 const supertest = require('supertest')
-const app = require('../app') // твое express-приложение
-const Blog = require('../models/blog')
+const app = require('../app')
+const Blog = require('../models/note')
 
 const api = supertest(app)
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-
+describe.only('NoTest', () => {
   const initialBlogs = [
     { title: 'First blog', author: 'Author1', url: 'http://example.com/1', likes: 1 },
     { title: 'Second blog', author: 'Author2', url: 'http://example.com/2', likes: 2 }
   ]
+  beforeEach(async () => {
+    await Blog.deleteMany({})
 
-  await Blog.insertMany(initialBlogs)
+    await Blog.insertMany(initialBlogs)
+  })
+
+  after(async () => {
+    await mongoose.connection.close()
+  })
+
+  test('blogs are returned as json and correct number', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    assert.strictEqual(response.body.length, initialBlogs.length)
+  })
 })
 
-test('blogs are returned as json and correct number', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  expect(response.body).toHaveLength(2)
-})
-
-afterAll(async () => {
-  await mongoose.connection.close()
-})
+// test('testTest', () => {
+//   expect(2 + 2).equal(4)
+// })
