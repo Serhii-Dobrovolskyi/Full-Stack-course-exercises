52d186f135a2dc3130aff500d11b7553e42e641d
Serhii Dobrovolskyi <serhiidobrovolskyi1@gmail.com>
2025-05-27 15:01:41 +0300

    added task 4.13

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -7,102 +7,102 @@ const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-describe.only('NoTest', () => {
-  const initialBlogs = [
-    { title: 'First blog', author: 'Author1', url: 'http://example.com/1', likes: 1 },
-    { title: 'Second blog', author: 'Author2', url: 'http://example.com/2', likes: 2 }
-  ]
+const initialBlogs = [
+  { title: 'First blog', author: 'Author1', url: 'http://example.com/1', likes: 1 },
+  { title: 'Second blog', author: 'Author2', url: 'http://example.com/2', likes: 2 }
+]
+
+describe('when there are initially some blogs saved', () => {
   beforeEach(async () => {
     await Blog.deleteMany({})
-
     await Blog.insertMany(initialBlogs)
   })
 
-  after(async () => {
-    await mongoose.connection.close()
+  describe('retrieving blogs', () => {
+    test('blogs are returned as json and correct number', async () => {
+      const response = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      assert.strictEqual(response.body.length, initialBlogs.length)
+    })
+
+    test('blog posts have id property instead of _id', async () => {
+      const response = await api.get('/api/blogs')
+      const blog = response.body[0]
+
+      assert.ok(blog.id)
+      assert.strictEqual(blog._id, undefined)
+    })
   })
 
-  test('blogs are returned as json and correct number', async () => {
-    const response = await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+  describe('adding a blog', () => {
+    test('a valid blog can be added', async () => {
+      const newBlog = {
+        title: 'test',
+        author: 'Serhii',
+        url: 'test',
+        likes: 22
+      }
 
-    assert.strictEqual(response.body.length, initialBlogs.length)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const response = await api.get('/api/blogs')
+      const authors = response.body.map(r => r.author)
+
+      assert.strictEqual(response.body.length, initialBlogs.length + 1)
+      assert(authors.includes('Serhii'))
+    })
+
+    test('if likes property is missing, it will default to 0', async () => {
+      const newBlog = {
+        title: 'Blog without likes',
+        author: 'Serhii',
+        url: 'testUrl'
+      }
+
+      const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      assert.strictEqual(response.body.likes, 0)
+    })
+
+    test('fails with 400 if title is missing', async () => {
+      const newBlog = {
+        author: 'Serhii',
+        url: 'testUrl',
+        likes: 5
+      }
+
+      await api.post('/api/blogs').send(newBlog).expect(400)
+
+      const response = await api.get('/api/blogs')
+      assert.strictEqual(response.body.length, initialBlogs.length)
+    })
+
+    test('fails with 400 if url is missing', async () => {
+      const newBlog = {
+        title: 'testTitle',
+        author: 'Serhii',
+        likes: 5
+      }
+
+      await api.post('/api/blogs').send(newBlog).expect(400)
+
+      const response = await api.get('/api/blogs')
+      assert.strictEqual(response.body.length, initialBlogs.length)
+    })
   })
+})
 
-  test('blog posts have id property instead of _id', async () => {
-    const response = await api.get('/api/blogs')
-    const blog = response.body[0]
-    assert.ok(blog.id)
-    assert.strictEqual(blog._id, undefined)
-  })
-  test('a valid blog can be added ', async () => {
-    const newBlog = {
-      title: 'test',
-      author: 'Serhii',
-      url: 'test',
-      likes: 22,
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-    const response = await api.get('/api/blogs')
-    const authors = response.body.map(r => r.author)
-    assert.strictEqual(response.body.length, initialBlogs.length + 1)
-    assert(authors.includes('Serhii'))
-
-  })
-  test('if likes property is missing, it will default to 0', async () => {
-    const newBlog = {
-      title: 'Blog without likes',
-      author: 'Serhii',
-      url: 'testUrl'
-    }
-
-    const response = await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-
-    if (response.body.likes !== 0) {
-      throw new Error(`Expected likes to be 0, but got ${response.body.likes}`)
-    }
-  })
-  test('if title is missing', async () => {
-    const newBlog = {
-      author: 'Serhii',
-      url: 'testUrl',
-      likes: 5
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
-
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, initialBlogs.length)
-  })
-  test('if url is missing', async () => {
-    const newBlog = {
-      title: "testTitle",
-      author: 'Serhii',
-      likes: 5
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
-
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, initialBlogs.length)
-  })
-
+after(async () => {
+  await mongoose.connection.close()
 })
--- part4/bloglist/utils/logger.js
+++ part4/bloglist/utils/logger.js
@@ -1,4 +1,15 @@
-const info = (...params) => console.log(...params)
-const error = (...params) => console.error(...params)
+const info = (...params) => {
+   if (process.env.NODE_ENV !== 'test') {
+      console.log(...params)
+   }
+}
 
-module.exports = { error, info }
+const error = (...params) => {
+   if (process.env.NODE_ENV !== 'test') {
+      console.error(...params)
+   }
+}
+
+module.exports = {
+   info, error
+}
