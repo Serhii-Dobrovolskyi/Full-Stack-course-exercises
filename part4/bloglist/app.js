0fd73cf86f9e51fdfb64af2a9bea0cf09e136ce6
Serhii Dobrovolskyi <serhiidobrovolskyi1@gmail.com>
2025-06-18 17:42:48 +0300

    updated task 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,24 +1,26 @@
-   const express = require('express')
-   const mongoose = require('mongoose')
-   const blogRouter = require('./controllers/blogs')
+const express = require('express')
+const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
-   const config = require('./utils/config')
-   const logger = require('./utils/logger')
-   const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
-   const app = express()
-   logger.info('connecting to', config.MONGODB_URI)
-   mongoose.connect(config.MONGODB_URI).then(() => logger.info('Connected to MongoDB')).catch(err => {
-      logger.error('MongoDB connection error:', err.message)
-   })
+const app = express()
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI).then(() => logger.info('Connected to MongoDB')).catch(err => {
+   logger.error('MongoDB connection error:', err.message)
+})
 
-   app.use(express.static('dist'))
-   app.use(express.json())
-   app.use(middleware.requestLogger)
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
 
-   app.use('/api/blogs', blogRouter)
+app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
 
-   app.use(middleware.unknownEndpoint)
-   app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-   module.exports = app
+module.exports = app
