722f96330a5c44b3b29d58c1a7a1831b5c457541
Serhii Dobrovolskyi <serhiidobrovolskyi1@gmail.com>
2025-06-26 22:03:41 +0300

    added task 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const config = require('./utils/config')
 const logger = require('./utils/logger')
@@ -19,6 +20,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
