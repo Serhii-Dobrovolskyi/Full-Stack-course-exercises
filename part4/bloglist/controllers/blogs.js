0c1cdc86701201e607c43ca10dfbea1163020a42
Serhii Dobrovolskyi <serhiidobrovolskyi1@gmail.com>
2025-06-27 20:47:52 +0300

    added task 4.23

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,7 +1,5 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
 const middleware = require('../utils/middleware')
 
 blogRouter.get('/', async (request, response) => {
@@ -35,26 +33,21 @@ blogRouter.put('/:id', async (request, response) => {
 blogRouter.post('/', middleware.userExtractor, async (request, response) => {
    const body = request.body
 
-
-   try {
-      const user = request.user
-      if (!user) {
-         return response.status(400).json({ error: 'userId missing or not valid' })
-      }
-      const blog = new Blog({
-         title: body.title,
-         author: body.author,
-         url: body.url,
-         likes: body.likes,
-         user: user._id
-      })
-      const savedBlog = await blog.save()
-      user.blogs = user.blogs.concat(savedBlog._id)
-      await user.save()
-      response.status(201).json(savedBlog)
-   } catch (error) {
-      response.status(400).json({ error: error.message })
+   const user = request.user
+   if (!user) {
+      return response.status(400).json({ error: 'userId missing or not valid' })
    }
+   const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id
+   })
+   const savedBlog = await blog.save()
+   user.blogs = user.blogs.concat(savedBlog._id)
+   await user.save()
+   response.status(201).json(savedBlog)
 })
 
 
