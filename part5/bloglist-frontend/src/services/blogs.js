3169d50716301d5a6ab11756fb70a7139c3b180e
Serhii Dobrovolskyi <serhiidobrovolskyi1@gmail.com>
2025-07-10 16:59:40 +0300

    updated task 5.4

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -12,7 +12,6 @@ const getAll = () => {
   return request.then(response => response.data)
 }
 const create = async newObj => {
-  console.log('Creating blog with token:', token);
   const config = {
     headers: { Authorization: token }
   }
