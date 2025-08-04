a24f9c11555fc34838490d5559194fe0fd385b52
Serhii Dobrovolskyi <serhiidobrovolskyi1@gmail.com>
2025-08-04 10:38:11 +0300

    added task 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,43 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{" "}
+        <button onClick={() => setShowAll(!showAll)}>
+          {showAll ? "hide" : "view"}
+        </button>
+      </div>
+      {showAll && (
+        <>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes} <button>like</button></div>
+          <div>{blog.user.username}</div>
+        </>
+      )}
+    </div>
+  );
+  // return (
+  //   <div style={blogStyle}>
+  //     <div>
+  //       {blog.title} {blog.author}{" "}
+  //       <button onClick={() => setShowAll(!showAll)}>hide</button>
+  //     </div>
+  //     <div>{blog.url}</div>
+  //     <div>{blog.likes}</div>
+  //     <div>{blog.user.username}</div>
+  //   </div>
+  // );
+};
+
+export default Blog;
