5514c60ff8dfa6bd56fe9b16957fd8c654a92f42
Serhii Dobrovolskyi <serhiidobrovolskyi1@gmail.com>
2025-08-05 12:17:45 +0300

    added task 5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -100,10 +100,11 @@ const App = () => {
       <Togglable buttonLabel="new blog" ref={blogFormRef}>
         <BlogForm createBlog={addBlog} />
       </Togglable>
-
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} onUpdateBlog={handleLikeUpdate} />
-      ))}
+      {blogs
+        .sort((a, b) => a.likes - b.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} onUpdateBlog={handleLikeUpdate} />
+        ))}
     </div>
   );
 };
