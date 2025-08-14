0d27a1fd94c013bbd7099088519490f45373e426
Serhii Dobrovolskyi <serhiidobrovolskyi1@gmail.com>
2025-08-14 10:34:59 +0300

    added task 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Blog from "./Blog";
 import { expect } from "vitest";
+import userEvent from "@testing-library/user-event";
 
 test("blog renders the title & author, but not URL or number of likes", () => {
   const blog = {
@@ -16,4 +17,25 @@ test("blog renders the title & author, but not URL or number of likes", () => {
   expect(screen.getByTestId("blog-author")).toBeInTheDocument();
   expect(screen.queryByTestId("blog-url")).toBeNull();
   expect(screen.queryByTestId("blog-likes")).toBeNull();
+});
+
+test("URL and likes are shown when the button controlling the shown details has been clicked", async () => {
+  const blog = {
+    title: "blogTitle",
+    author: "blogAuthor",
+    url: "blogUrl",
+    likes: 11,
+    user: { username: "owner", id: "123" },
+  };
+  const currentUser = { username: "someone" };
+
+  render(<Blog blog={blog} user={currentUser} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText("view");
+
+  await user.click(button);
+
+  expect(screen.getByTestId("blog-url")).toBeInTheDocument();
+  expect(screen.getByTestId("blog-likes")).toBeInTheDocument();
 });
