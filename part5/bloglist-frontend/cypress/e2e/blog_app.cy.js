863855c4f3e52d9c4b7895ff758a0996382d928d
Serhii Dobrovolskyi <serhiidobrovolskyi1@gmail.com>
2025-09-17 11:47:34 +0300

    updated task 5.18

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,8 +1,6 @@
+
 describe('Blog app', function () {
   beforeEach(function () {
-    // cy.get('#username').type('serh')
-    // cy.get('#password').type('lavr444')
-    // cy.contains('login').click()
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
 
     const user = {
@@ -13,8 +11,25 @@ describe('Blog app', function () {
     cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:5173/')
   })
-  it('Login form is shown', function () {
-    cy.contains('to application')
 
+  it('Login form is shown', function () {
+    cy.get('button').contains('login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('serh')
+      cy.get('#password').type('lavr444')
+      cy.contains('login').click()
+      cy.contains('Serhii Dobrovol logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('serh')
+      cy.get('#password').type('lavr44')
+      cy.contains('login').click()
+      cy.contains('Wrong credentials')
+      cy.contains('Serhii Dobrovol logged in').should('not.exist')
+    })
   })
 })
